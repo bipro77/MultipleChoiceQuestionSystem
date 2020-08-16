03b0a0df9700ffd67f2061ed8f881bce86f05f49
Biprojit Mondal <bipro77@gmail.com>
2020-08-16 13:25:16 +0600

    MCQS setup 4

---
+++ src/main/java/com/fireflies/mcqs/service/UserDetailsServiceImpl.java
@@ -0,0 +1,66 @@
+package com.fireflies.mcqs.service;
+
+import com.fireflies.mcqs.model.Role;
+import com.fireflies.mcqs.model.User;
+import com.fireflies.mcqs.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+
+
+//    public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException, DataAccessException {
+//        // returns the get(0) of the user list obtained from the db
+//        User user = userRepository.findByUsername(name);
+//
+//
+//        Set<Role> roles = user.getRoles();
+////        logger.debug("role of the user" + roles);
+//
+//        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+//        for(Role role: roles){
+//            authorities.add(new SimpleGrantedAuthority(role.getName()));
+////            logger.debug("role" + role + " role.getRole()" + (role.getRole()));
+//        }
+//
+//        UserDetails userDetails= new UserDetails();
+////        userDetails.setUser(user);
+////        userDetails.setAuthorities(authorities);
+//
+//        return userDetails;
+//
+//    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+//        User user = userRepository.findByEmail(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not authorized.");
+        }
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        for (Role role : user.getRoles()){
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+
+//        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAuthorities);
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.isAccountEnabled(), true, true, true, grantedAuthorities);
+    }
+}
